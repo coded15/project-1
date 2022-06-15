73a58a073b3b57b340854f6466271b70f4711bb7
coded15 <91874598+coded15@users.noreply.github.com>
2022-06-16 02:55:49 +0530

    added content

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+export default function Content() {
+    return (
+        <div className="content">
+            <h2>About</h2>
+            <h3>
+                I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </h3>
+            <h2>Interests</h2>
+            <h3>
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </h3>
+        </div>
+    )
+}

733280dc977f5d9d7e42c01c1d8ecb74af098de4
coded15 <91874598+coded15@users.noreply.github.com>
2022-06-16 02:56:29 +0530

    updated main for button, content and footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,18 @@
 import cardLogo from "../images/Ichchha2.jpeg"
 import Header from "../components/Header"
+import Button from "./Buttons"
+import Footer from "../components/Footer"
+import Content from "../components/Content"
 export default function Main() {
     return (
         <div className="outer-box">
             <img src={cardLogo} alt="my-pic" className="my-image" />
-            < Header />
-
+            <div className="inner-box">
+                <Header />
+                <Button />
+                <Content />
+                <Footer />
+            </div>
         </div>
     )
 }
